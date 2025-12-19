0880940e459fefe552e6192cc5ff34b0fa6b9b27
FixerCoda <bayessantosdiego@gmail.com>
2025-12-18 23:41:18 -0500

    Agregar ejemplo básico de Delegates

---
+++ Delegates/BasicDelegates.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace BasicDelegates
+{
+    class Program
+    {
+        delegate void LogDel(string text);
+
+        static void Main(string[] args)
+        {
+            LogDel logDel = new LogDel(LogTextToScreen);
+
+            Console.WriteLine("Please enter your name");
+            var name = Console.ReadLine();
+
+            logDel(name);
+
+            Console.ReadKey();
+        }
+
+        static void LogTextToScreen(string text)
+        {
+            Console.WriteLine($"{DateTime.Now}: {text}");
+        }
+    }
+}
